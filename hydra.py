fcc9a7eaecce88b58650d473c7fa74b6e6f09fba
zhaozhe <178730210@qq.com>
2017-05-07 21:43:43 +0800

    提交后端接口

---
+++ hydra.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+
+from games.recognizer import Recognizer
+from user import User
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+api.add_resource(Recognizer,'/games/recognizer')
+api.add_resource(User,'/users')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
---
+++ user.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource
+
+class User(Resource):
+    def get(self):
+        return [{
+            'name': 'zhaozhe',
+            'email': 'zzl1787@gmail.com',
+            'website': 'www.imzhaozhe.com'
+        }]
