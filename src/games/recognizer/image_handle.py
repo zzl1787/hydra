e8911af9f870a152507d14e66c015d9b2e4bf4c9
zzl1787 <zzl1787@gmail.com>
2017-05-08 19:46:00 +0800

    -

--- src/games/recognizer/image_handle.py
+++ src/games/recognizer/image_handle.py
@@ -2,18 +2,18 @@ from PIL import Image
 import numpy as np
 
 
-def image_to_string(file_path):
+def image_to_matrix(file_path):
     image = Image.open(file_path)
-    # image.show()
     width,height = image.size
     # image = image.convert("L")
-    # image.show()
-    data = image.getdata()
-    # data = np.matrix(data, dtype='float')/255.0
-    # newdata = np.reshape(data,(width,height))
-    # print newdata
-    return
+    print width,"  ",height
+    data = image.getdata(0)
+    print len(list(data))
+    print list(data)
+    data = np.matrix(data, dtype='float')/255.0
+    newdata = np.reshape(data,(width,height))
+    return newdata
 
 
 if __name__ == '__main__':
-    image_to_string(u"z:\\yay.jpg")
+    image_to_matrix(u"z:\\yay.jpg")
